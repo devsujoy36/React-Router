1f768f6be00e1f46bdd65fc9aa184cc22007fa59
unknown <sujoydaspc2023@gmail.com>
2024-12-20 19:00:19 +0600

    changes

--- src/Component/Header/Header.jsx
+++ src/Component/Header/Header.jsx
@@ -1,6 +1,7 @@
+/* eslint-disable react/prop-types */
 import { NavLink } from "react-router-dom"
 import './Header.css'
-const Header = () => {
+const Header = ({ navClickHandler }) => {
     return (
         <div className="shadow-xl bg-emerald-500">
             <div className="flex  md:flex-row gap-4 flex-col justify-between items-center max-w-screen-xl mx-auto p-5  text-white">
@@ -8,11 +9,11 @@ const Header = () => {
                 <a href="" className='text-4xl font-semibold cursor-pointer'>Home.jsx</a>
 
                 <nav id="nav-bar" className="flex gap-5 md:gap-10 font-semibold text-xl ">
-                    <NavLink to="/" className="hover:text-black active:scale-95 transition-all">Home</NavLink>
-                    <NavLink to="/about" className="hover:text-black active:scale-95 transition-all">About</NavLink>
-                    <NavLink to="/users" className="hover:text-black active:scale-95 transition-all">Users</NavLink>
-                    <NavLink to="/posts" className="hover:text-black active:scale-95 transition-all">Posts</NavLink>
-                    <NavLink to="/contact" className="hover:text-black active:scale-95 transition-all">Contact</NavLink>
+                    <button className="hover:text-black active:scale-95 transition-all" ><NavLink to="/" >Home</NavLink></button>
+                    <button className="hover:text-black active:scale-95 transition-all" onClick={navClickHandler}><NavLink to="/about">About</NavLink></button>
+                    <button className="hover:text-black active:scale-95 transition-all" onClick={navClickHandler}><NavLink to="/users">Users</NavLink></button>
+                    <button className="hover:text-black active:scale-95 transition-all" onClick={navClickHandler}><NavLink to="/posts">Posts</NavLink></button>
+                    <button className="hover:text-black active:scale-95 transition-all" onClick={navClickHandler}><NavLink to="/contact">Contact</NavLink></button>
                 </nav>
 
             </div>
--- src/Component/Home/Home.jsx
+++ src/Component/Home/Home.jsx
@@ -2,22 +2,37 @@ import { Outlet, useLocation, useNavigation } from "react-router-dom"
 import Header from "../Header/Header"
 import Footer from "../Footer/Footer"
 import Loading from "../Loading/Loading"
+import { useState } from "react"
 
 const Home = () => {
-const navigation = useNavigation()
-const location = useLocation()
-console.log(location);
+    const navigation = useNavigation()
+    const location = useLocation()
+    console.log(location);
+
+
+    const [toggle, setToggle] = useState(true);
+
+    const navClickHandler = () => {
+        setToggle(false)
+    }
 
     return (
         <div className="">
-            <Header/>
+            <Header navClickHandler={navClickHandler} />
             {
-                navigation.state === "loading"?<Loading/>:<Outlet />
+                toggle &&
+                <div className="min-h-[76vh] bg-black text-white flex justify-center gap-2 flex-col items-center">
+                    <h1 className="text-5xl font-bold">Wellcome To Our</h1>
+                    <h1 className="text-4xl  font-bold">React Route</h1>
+                </div>
             }
             
-            
-            <Footer/>
-            
+            {
+                navigation.state === "loading" ? <Loading /> : <Outlet />
+            }
+
+            <Footer />
+
         </div>
     )
 }
